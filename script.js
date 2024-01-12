a9bddccb7c19f2651ff690a6dd9987157c485e77
Mango <143851144+MangoIsHere@users.noreply.github.com>
2024-01-11 20:04:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,18 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
   paintColor = getRandomColor();
   resetPainting();
 
-  setInterval(resetPainting, 5000);
-
-  function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  setTimeout(function() {
+  canvas.addEventListener('click', function() {
     const redirectTo = 'OFFLINE'; // REPLACE WITH URL
 
     if (isValidUrl(redirectTo)) {
@@ -53,7 +42,16 @@ document.addEventListener('DOMContentLoaded', function() {
       showAlertAndClose();
       displayNotFoundMessage();
     }
-  }, 10000);
+  });
+
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
   function isValidUrl(url) {
     try {
